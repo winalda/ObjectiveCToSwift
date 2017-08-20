320a32a93ef3794ba0a08a24032ae79833ec9f4c
winalda <alberto@kokonutstudio.com>
2017-08-20 11:01:21 -0500

    Initial Commit

---
+++ ObjectiveCToSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ObjectiveCToSwift
+//
+//  Created by Erick Alberto Morales Ledesma on 20/08/17.
+//  Copyright © 2017 Erick Alberto Morales Ledesma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
